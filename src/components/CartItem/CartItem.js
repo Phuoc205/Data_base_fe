7c47d7680d97592d5c3b96e8fba3b061b9e1db24
Phuoc205 <tanphuoc120605@gmail.com>
2025-04-24 23:26:00 +0700

    modify cart

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,93 +1,49 @@
-import React, { useState , useEffect} from "react";
+import React from "react";
 import './CartItem.css';
 
 /*  Các tham số truyền vào
-    provider: nhà cung cấp
+    manufacture: nhà cung cấp
     img_link: link ảnh
     info: thông tin
     price: giá
     amount: số lượng
+    checked: tick
 */
 
-
 function CartItem(props) {
-
-    const [amount, setAmount] = useState(props.amount)
-    const [inputAmount, setInputAmount] = useState(props.amount)
-
-    useEffect(() => {
-        setAmount(props.amount);
-        setInputAmount(props.amount);
-    }, [props.amount]);
-
-    const handleAmountChange = (newAmount) => {
-        const finalAmount = Math.max(newAmount, 1);
-        setAmount(finalAmount);
-        setInputAmount(finalAmount);
-    };
-
-    const handleInputChange = (e) => {
-        const value = e.target.value;
-        if(!isNaN(value)) {
-            setInputAmount(value);
-            setAmount(value)
-        }
-    }
-
-    const handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            const finalAmount = parseInt(inputAmount, 10);
-            if (!isNaN(finalAmount)) {
-                setAmount(finalAmount);
-                props.onAmountChange && props.onAmountChange(finalAmount);
-            }
-        }
-    }
-
     return (
         <div className="cart-item">
             <div className="cart-item-header">
-                <div className="tick-check"><input type="checkbox"/></div>
+                <div className="tick-check">
+                    <input
+                        type="checkbox"
+                        checked={props.checked}
+                        onChange={props.onCheck}
+                    />
+                </div>
                 <i className="fa-solid fa-store store-icon"></i>
-                {props.provider}
+                {props.manufacture}
             </div>
             <div className="cart-item-body">
-                <div className="tick-check"><input type="checkbox" className="buy_check"/></div>
-                <img src={props.img_link} className="cart-item-img"></img>
-
+                <div className="tick-check">
+                    <input
+                        type="checkbox"
+                        className="buy_check"
+                        checked={props.checked}
+                        onChange={props.onCheck}
+                    />
+                </div>
+                <img src={props.img_link} className="cart-item-img" />
                 <div className="cart-info-container">
                     <p className="cart-item-info">{props.info}</p>
-                    {props.provider}
+                    {props.manufacture}
                 </div>
-
                 <div className="cart-price-container">
-                    {(amount*props.price).toLocaleString()}
-                    <div className="cart-price-icon-group">
-                        <i className="fa-solid fa-heart cart-price-icon"></i>
-                        <i className="fa-solid fa-trash cart-price-icon"></i>
-                    </div>
-                </div>
-                
-                <div className="cart-amount-container">
-                    <div className="amount-icon" onClick={() => handleAmountChange(amount-1)}>
-                        <i className="fa-solid fa-minus"></i>
-                    </div>
-                    <div className="amount-number">
-                        <input 
-                            className="input-amount"
-                            value={inputAmount}
-                            onChange={handleInputChange}
-                            onKeyDown={handleKeyDown}
-                            placeholder={amount}
-                        />
-                    </div>
-                    <div className="amount-icon" onClick={() => handleAmountChange(amount+1)}>
-                        <i className="fa-solid fa-plus"></i>
-                    </div>
+                    {(props.amount * props.price).toLocaleString()}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default CartItem
